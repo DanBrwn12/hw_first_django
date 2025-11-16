f26ce9b34c267e5366266395bd591a781323f5bd
Vladimir <daway4554@gmail.com>
2025-11-16 18:25:21 +0300

    Домашнее задание "Работа с ORM"

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -13,5 +13,13 @@ class Command(BaseCommand):
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            data = Phone.objects.create(
+                name=phone['name'],
+                image=phone['image'],
+                price=phone['price'],
+                release_date=phone['release_date'],
+                lte_exists=phone['lte_exists'],
+            )
+            self.stdout.write(self.style.SUCCESS(f"Добавлен: {data.name}"))
+
+
