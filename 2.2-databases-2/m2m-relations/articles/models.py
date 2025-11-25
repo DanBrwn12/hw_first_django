6338b3d8d99eb28d6c0ffec8e12764a547e30fff
Vladimir <daway4554@gmail.com>
2025-11-25 20:48:14 +0300

    Домашнее задание "Работа с ORM, часть 2" Часть 2

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+class Tag(models.Model):
+
+    name = models.CharField(max_length=50, verbose_name='Раздел')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Раздел'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
 
@@ -7,10 +19,24 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tags = models.ManyToManyField(Tag, through='Scope', related_name='articles')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+class Scope(models.Model):
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False, verbose_name='Основной раздел')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематика статьи'
+        unique_together = ['article', 'tag']
+        ordering = ['-is_main', 'tag__name']
